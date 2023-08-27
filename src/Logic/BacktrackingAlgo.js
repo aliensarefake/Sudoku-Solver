ee39e5e843b5681ac542bcf5996c21e186bb76a9
Guo Xiao Yu <guoxiaoyu2003429@gmail.com>
2023-08-27 20:58:21 +0800

    Create BacktrackingAlgo.js

---
+++ src/Logic/BacktrackingAlgo.js
@@ -0,0 +1,42 @@
+function isValid(board, row, col, num) {
+    // Check row
+    for (let i = 0; i < 9; i++) {
+      if (board[row][i] === num) return false;
+    }
+  
+    // Check column
+    for (let i = 0; i < 9; i++) {
+      if (board[i][col] === num) return false;
+    }
+  
+    // Check 3x3 grid
+    let startRow = row - (row % 3), startCol = col - (col % 3);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i + startRow][j + startCol] === num) return false;
+      }
+    }
+    
+    return true;
+  }
+  
+  function solveSudoku(board) {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (board[row][col] === 0) {
+          for (let num = 1; num <= 9; num++) {
+            if (isValid(board, row, col, num)) {
+              board[row][col] = num;
+              if (solveSudoku(board)) {
+                return true;
+              }
+              board[row][col] = 0; // backtrack
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true; // Sudoku solved
+  }
+  
